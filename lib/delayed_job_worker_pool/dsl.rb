d02d9fc314e3b6a6d412b644403fdf6dfdd812a1
Severin Räz <raez@puzzle.ch>
2020-05-11 10:31:26 +0200

    Draft an approach to implement multi pool dsl

--- lib/delayed_job_worker_pool/dsl.rb
+++ lib/delayed_job_worker_pool/dsl.rb
@@ -1,25 +1,37 @@
 module DelayedJobWorkerPool
   class DSL
-    SIMPLE_SETTINGS = [:workers, :queues, :min_priority, :max_priority, :sleep_delay, :read_ahead].freeze
+    class ConflictingWorkerConfiguration < StandardError; end
+    class ContextError < StandardError; end
+
+    POOL_SETTINGS = [:workers, :queues, :min_priority, :max_priority, :sleep_delay, :read_ahead].freeze
     CALLBACK_SETTINGS = [:after_preload_app, :on_worker_boot, :after_worker_boot, :after_worker_shutdown].freeze
 
     def self.load(path)
-      options = {}
-
-      dsl = new(options)
-      dsl.instance_eval(File.read(path), path, 1)
-
-      options
+      new.instance_eval(File.read(path), path, 1).to_h
     end
 
-    def initialize(options)
-      @options = options
+    def initialize
+      @options = {}
+      @worker_pools = []
+      @mode = nil
     end
 
-    SIMPLE_SETTINGS.each do |option_name|
+    POOL_SETTINGS.each do |option_name|
       define_method(option_name) do |option_value|
-        @options[option_name] = option_value unless option_value.nil?
+        default_to_mode(:implicit_pool)
+        set_pool_option(option_name, option_value) unless option_value.nil?
       end
+    end
+
+    def worker_pools(&block)
+      use_mode!(:explicit_pools)
+      yield
+    end
+
+    def worker_pool(&block)
+      @current_pool = add_worker_pool
+      yield
+      @current_pool = nil
     end
 
     def preload_app(preload_app = true)
@@ -30,6 +42,41 @@ module DelayedJobWorkerPool
       define_method(option_name) do |&block|
         @options[option_name] = block
       end
+    end
+
+    def to_h
+      @options.merge(worker_pools: @worker_pools)
+    end
+
+    private
+
+    def set_pool_option(name, value)
+      if @current_pool.nil?
+        raise ContextError,
+              'Are you mixing implicit and explicit worker configurations? Is your configration nested incorrectly?'
+      end
+      @current_pool[name] = value
+    end
+
+    def default_to_mode(mode)
+      use_mode!(mode) if @mode.nil?
+    end
+
+    def use_mode!(mode)
+      return if @mode == mode
+      unless @mode.nil?
+        raise ConflictingWorkerConfiguration,
+              'You cannot mix configrations of implicit and explicit worker pool mode.'
+      end
+
+      @mode = mode
+      @current_pool = add_worker_pool if @mode == :implicit_pool
+    end
+
+    def add_worker_pool
+      options = {}
+      @worker_pools << options
+      options
     end
   end
 end
